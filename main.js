e81dbbc1d22fb245a59d71018657b95e2817cf53
artegrr <foster2196@gmail.com>
2020-05-21 20:35:08 +0300

    task 2 ?

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+//Task 2:
+// DNA_strand ("ATTGC") # return "TAACG"
+// DNA_strand ("GTAT") # return "CATA"
+
+// function DNA_strand(dna) {
+//     let letters = { 'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C' };
+//     let arr = [];
+//     for (let i = 0; i < dna.length; i++) {
+//         arr[i] = letters[dna[i]];
+//     }
+
+//     return arr.join('');
+// }
+
+
+
+
 // Task 3: Светофор
 let i = -1;
 
